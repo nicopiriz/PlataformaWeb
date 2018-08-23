dc47d1da330fdcb80558f65c058e9f7c65a74d48
nicopiriz <42629586+nicopiriz@users.noreply.github.com>
2018-08-22 23:58:17 -0300

    Add files via upload

---
+++ PlataformaJuegos/src/plataformajuegos/Consola.java
@@ -0,0 +1,23 @@
+package plataformajuegos;
+
+public class Consola {
+    private int idConsola;
+    private String nombre;
+    
+    //Constructor
+    public Consola(){
+    }
+    //Getters and Setters
+    public int getIdConsola() {
+        return idConsola;
+    }
+    public void setIdConsola(int idConsola) {
+        this.idConsola = idConsola;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
